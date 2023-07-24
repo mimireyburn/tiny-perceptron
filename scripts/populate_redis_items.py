6881c1d0b4f2213daf3ee882c30e981465d2c72e
Mimi Reyburn <mimireyburn@gmail.com>
2023-07-24 11:12:08 +0100

    Change to Bes's items to redis script

--- scripts/populate_redis_items.py
+++ scripts/populate_redis_items.py
@@ -1,58 +1,30 @@
 import psycopg2
+import datetime
 import redis
 
-def fetch_items_from_postgres():
-    # PostgreSQL connection details
-    pg_host = "localhost"
-    pg_port = "5432"
-    pg_database = "W9sV6cL2dX"
-    pg_user = "root"
-    pg_password = "E5rG7tY3fH"
+r = redis.Redis(host="localhost", port=6379, db=1, password="MvY4bQ7uN3")
+connection = psycopg2.connect(host="localhost", user="root", port=5432, database="W9sV6cL2dX", password="E5rG7tY3fH")
 
-    # Redis connection details
-    redis_host = "localhost"
-    redis_port = "6379"
-    redis_password = "MvY4bQ7uN3"
 
-    try:
-        # Connect to PostgreSQL
-        pg_connection = psycopg2.connect(
-            host=pg_host,
-            port=pg_port,
-            database=pg_database,
-            user=pg_user,
-            password=pg_password
-        )
-        pg_cursor = pg_connection.cursor()
+with connection:
+  cur = connection.cursor("sync_redis_cursor")
+  cur.execute("SELECT item_key, created_at, user_id, type FROM items;")
+  batch_size = 5000
+  curr_counter = 0
 
-        # Connect to Redis
-        redis_connection = redis.StrictRedis(
-            host=redis_host,
-            port=redis_port,
-            db=1,
-            password=redis_password,
-            decode_responses=True
-        )
+  while True:
+    print("BATCH:", curr_counter)
+    items = cur.fetchmany(batch_size)
+    if not items: break
+    curr_counter += 1
+    pipe = r.pipeline()
 
-        # Fetch all items from PostgreSQL
-        pg_cursor.execute("SELECT items.item_key FROM items;")
-        items = pg_cursor.fetchall()
+    for item in items:
+      item_key, created_at, user_id, type = item
+      datetime_obj =  datetime.datetime.strptime(str(created_at), '%Y-%m-%d %H:%M:%S')
+      unix_timestamp = int(datetime_obj.timestamp())
+      item_key_redis = f"item:{item_key}"
+      pipe.hset(item_key_redis, mapping={"id": item_key, "user": user_id, "type": type, "unix": unix_timestamp})
 
-        # Add items to Redis
-        for item in items:
-            redis_connection.sadd("items_set", item[0])
-
-        print("Items successfully added to Redis!")
-
-    except (psycopg2.Error, redis.exceptions.RedisError) as e:
-        print(f"Error: {e}")
-
-    finally:
-        # Close connections
-        if pg_connection:
-            pg_connection.close()
-        if redis_connection:
-            redis_connection.close()
-
-if __name__ == "__main__":
-    fetch_items_from_postgres()
+    pipe.execute()
+  cur.close()
